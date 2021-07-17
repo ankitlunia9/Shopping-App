27ab0717645d07d53962c8d2aa86ba0be2f41aff
ankitlunia9 <luniankit1999@gmail.com>
2021-07-17 18:40:11 +0530

    Initial commit

---
+++ lib/screens/orders_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Orders'),
+      ),
+      drawer: AppDrawer(),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              return Center(
+                child: Text(
+                  'No Orders Found!',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
